4b4c60ac422b0022cc47b344e6e4ce0f65db67b9
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 09:40:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,7 @@ require'pry'
 def key_for_min_value(name_hash)
   #binding.pry
   new = name_hash.map {|key, value| value}
-  if new[0] > new[1]
-    new[0], new[1] = new[1], new[0]
-  elsif new[3] < new[2] && new[3] < new[1]
-    new[1], new[3] = new[3], new[1]
-  else 
-    nil
-  end 
-  name_hash[new[1]]
+  if new[0] < new[1] && new[0] < new[2]
+    name_hash[name[0]]
+  elsif name[1] < name[0] && name[1] < name[3]
 end
