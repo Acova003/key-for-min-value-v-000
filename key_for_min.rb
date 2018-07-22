b0ce071d33a6022b0df29e7d92a4ac387998c68b
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 09:48:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 require'pry'
 def key_for_min_value(name_hash)
-  #binding.pry
-  new = name_hash.map {|key, value| value}
-  if new[0] < new[1] && new[0] < new[2]
-    name_hash[name[0]]
-  elsif new[1] < new[0] && new[1] < new[2]
-    name_hash[name[1]]
-  elsif new[2] < new[0] && new[2] < new[1]
-    name_hash[new[2]]
-  else
-    nil 
-  end 
+  return nil if name_hash
 end
