1154be9d2563e31d7118540799392dc1dc092a1f
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 09:33:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,9 @@ require'pry'
 def key_for_min_value(name_hash)
   #binding.pry
   new = name_hash.map {|key, value| value}
-  if new[.length > 0] 
+  if new[0] > new[1]
+    new[0], new[1] = new[1], new[0]
+  elsif new[3]
     puts min
   else 
     nil 
