6cca67674c0d4545992c7bc7c3b73645a249487f
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 08:56:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require'pry'
 def key_for_min_value(name_hash)
+  
   sorted_v = []
   min = sorted_v.min
   name_hash.map {|n,v| sorted_v << v}
