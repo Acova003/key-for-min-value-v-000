b5a6f3cc3b5c358e5aaa407d12cd576ed383f91d
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 09:43:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,11 @@ def key_for_min_value(name_hash)
   new = name_hash.map {|key, value| value}
   if new[0] < new[1] && new[0] < new[2]
     name_hash[name[0]]
-  elsif name[1] < name[0] && name[1] < name[3]
+  elsif new[1] < new[0] && new[1] < new[2]
     name_hash[name[1]]
+  elsif new[2] < new[0] && new[2] < new[1]
+    name_hash[new[2]]
+  else
+    nil 
+  end 
 end
