8f0386f4563d1925b4568cd8fa2fc7d9cd329a78
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 09:54:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   return nil if name_hash.nil?
   keys = ""
   values = "" 
-  name_hash.each |key, value|
+  name_hash.each do |key, value|
   if values = ""
     values = value
     keys = key
